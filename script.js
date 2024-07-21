429a864093932f4448ff9f6e4b0e8d19385e1cc1
Marvin Vollmer <marvinjvollmer@gmail.com>
2024-07-21 11:04:59 +0200

    Basic HTML and CSS

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("Hello, world!");
+
+const textTitle_1 = document.querySelector("#title_1");
+const textTitle_2 = document.querySelector("#title_2");
+const textTitle_3 = document.querySelector("#title_3");
+const textTitle_4 = document.querySelector("#title_4");
+
+const textParagraph_1 = document.querySelector("textParagraph_1");
+const textParagraph_2 = document.querySelector("textParagraph_2");
+const textParagraph_3 = document.querySelector("textParagraph_3");
+const textParagraph_4 = document.querySelector("textParagraph_4");
